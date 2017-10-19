8e38043ab4fa1d3b697352a3705095e0d316fdbe
Qiagshen <shen295923@163.com>
2017-10-19 18:27:02 +0800

    [测试github账户]

--- app/src/main/java/com/banyue/mangostyle/MainActivity.kt
+++ app/src/main/java/com/banyue/mangostyle/MainActivity.kt
@@ -3,6 +3,7 @@ package com.banyue.mangostyle
 import android.os.Build
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import com.banyue.commonlib.module.utils.toast
 
 class MainActivity : AppCompatActivity() {
 
@@ -10,5 +11,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         Build.VERSION.SDK_INT
+        "test0".toast()
     }
 }
