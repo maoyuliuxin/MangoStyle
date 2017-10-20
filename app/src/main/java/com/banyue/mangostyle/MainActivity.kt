5f949cb538a44ce59a42290f67af1389ab600ea1
Qiagshen <shen295923@163.com>
2017-10-20 11:52:25 +0800

    [项目架构调试完成]

--- app/src/main/java/com/banyue/mangostyle/MainActivity.kt
+++ app/src/main/java/com/banyue/mangostyle/MainActivity.kt
@@ -3,7 +3,10 @@ package com.banyue.mangostyle
 import android.os.Build
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.widget.TextView
 import com.banyue.commonlib.module.utils.toast
+import kotlinx.android.synthetic.main.activity_main.*
+import org.jetbrains.anko.sdk25.coroutines.onClick
 
 class MainActivity : AppCompatActivity() {
 
@@ -12,5 +15,10 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         Build.VERSION.SDK_INT
         "test0".toast()
+        val textView = TextView(this)
+        textView.setOnClickListener {
+
+        }
+        text.onClick {  }
     }
 }
